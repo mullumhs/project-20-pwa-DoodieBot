353e28750ed6d2d412be40893b6a5bd174026197
DoodieBot <140124349+DoodieBot@users.noreply.github.com>
2024-11-20 10:40:52 +1100

    created data base model

--- models.py
+++ models.py
@@ -2,5 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# Define your database model here
-# Example: class Item(db.Model):
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    brand = db.Column(db.String(50), nullable=False)
+    model = db.Column(db.Text)
+    cc = db.Column(db.Date, nullable=False)
+    fuel_capacity = db.Column(db.Integer, nullable=False)
+    engine_type = db.Column(db.Boolean, default=False)
+    seat_height = db.Column(db.intiger, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Task {self.title}>'
