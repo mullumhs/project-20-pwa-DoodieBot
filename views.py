25ff1ff795d4eaab0df2e4254677498bcbc37cd2
DoodieBot <140124349+DoodieBot@users.noreply.github.com>
2024-12-13 10:39:45 +1100

    polished search function and did a little code comminting

--- views.py
+++ views.py
@@ -8,16 +8,26 @@ from models import db, Bike # Also import your database model here
 # You can also use flash for displaying status messages
 
 def init_routes(app):
-
+# to register home page and print things fromdata base
     @app.route('/', methods=['GET'])
     def get_items():
-        # This route should retrieve all items from the database and display them on the page.
-        bikes = Bike.query.all()
-        print(bikes)
+        search_query = request.args.get('query')
+    
+        if search_query:
+            print (search_query)
+            bikes = Bike.query.filter(Bike.brand.ilike(f'%{search_query}%')).all()
+        else:
+            bikes = Bike.query.all()
         return render_template('index.html', bikes=bikes)
 
+# for searching and querying items from the data base
+    @app.route('/search', methods=['GET'])
+    def bike_search():
+        if request.method == 'post':
+            query = request.form['search']
+            return redirect(url_for('get_items', query = query ))
 
-
+# adding an item to the data base
     @app.route('/add', methods=['GET','POST'])
     def create_item():
         if request.method == 'POST':
@@ -35,12 +45,12 @@ def init_routes(app):
         return render_template('add.html', message='Item added successfully')
 
 
-
+# dealing with editing an existing item inj the data base 
     @app.route('/edit', methods=['GET','POST'])
     def edit():
 
         
-        
+     # if statement to gather all atributs of an item   
         if request.method == 'POST':
             id = request.form['id']
             print(id)
@@ -61,7 +71,7 @@ def init_routes(app):
         return render_template('edit.html', bike = bike)
 
 
-
+# delete function 
     @app.route('/delete', methods=['GET'])
     def delete_item():
         id = request.args.get("id")
@@ -73,23 +83,9 @@ def init_routes(app):
 
 
 
-    @app.route('/search', methods=['POST'])
-    def search():
-        model = request.form['model']
-        id = request.args.get("id")
-        bike = db.get_or_404(Bike, id)
 
         
-        
-    @app.route('/', methods=['GET'])
-    def get_items():
-        search_query = request.args.get('query')
-    
-        if search_query:
-            items = YourModel.query.filter(YourModel.title.ilike(f'%{search_query}%')).all()
-        else:
-            items = YourModel.query.all()
-        return render_template('index.html', items=items)
+
 
 
 
